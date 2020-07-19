8d37f2225059d2bdf3f41c599174a188843755f5
Spimy <officialspyme@gmail.com>
2020-07-19 21:50:08 +0400

    Add login url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
-from .views import RegisterView
+from django.urls import path
+from .views import RegisterView, LoginView
 
 app_name = 'authentication'
 urlpatterns = [
-    url('register/', RegisterView.as_view(), name='register')
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
